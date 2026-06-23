8bf13b2051d18e4a157d8f0abd47c746e098470c
Mayur <kupademayurm384@gmail.com>
2026-06-23 15:20:33 +0530

    Implemented Parallel Test Execution

--- src/test/java/base/PlaywrightManager.java
+++ src/test/java/base/PlaywrightManager.java
@@ -8,64 +8,75 @@ import com.microsoft.playwright.Playwright;
 
 public class PlaywrightManager {
 	
-	private static Playwright playwright;
-	private static Browser browser;
-	private static BrowserContext context;
-	private static Page page;
+	private static ThreadLocal<Playwright> playwright =
+			new ThreadLocal<>();
+	
+	private static ThreadLocal<Browser> browser =
+			new ThreadLocal<>();
+	
+	private static ThreadLocal<BrowserContext> context =
+			new ThreadLocal<>();
+	
+	private static ThreadLocal<Page> page =
+			new ThreadLocal<>();
 	
 	public static Page getPage(String browserName,
 								boolean headless) {
 		
-		playwright = Playwright.create();
+		playwright.set(Playwright.create());
 		
 		switch(browserName.toLowerCase()) {
 		
 		case "firefox":
 			
-			browser = 
-					playwright.firefox()
-					.launch(new BrowserType
-							.LaunchOptions()
-							.setHeadless(headless));
+			browser.set(
+					playwright.get()
+					.firefox()
+					.launch(new BrowserType.LaunchOptions()
+							.setHeadless(headless)));
 			
 			break;
 			
 		case "webkit":
 			
-			browser =
-					playwright.webkit()
-					.launch(new BrowserType
-							.LaunchOptions()
-							.setHeadless(headless));
+			browser.set(
+					playwright.get()
+					.webkit()
+					.launch(new BrowserType.LaunchOptions()
+							.setHeadless(headless)));
 					
 					break;
 			
 			default:
 				
-				browser = 
-						playwright.chromium()
-						.launch(new BrowserType
-								.LaunchOptions()
-								.setHeadless(headless));		
+				browser.set(
+						playwright.get()
+						.chromium()
+						.launch(new BrowserType.LaunchOptions()
+								.setHeadless(headless)));		
 		}
 		
-		context = browser.newContext();
-		
-		page = context.newPage();
-		
-		return page;
+		context.set(browser.get().newContext());
+
+        page.set(context.get().newPage());
+
+        return page.get();
+	}
+	
+	public static Page getCurrentPage() {
+        return page.get();
 	}
 	
 	public static void closeBrowser() {
-		
-		if(context != null)
-			context.close();
-		
-		if(browser != null)
-			browser.close();
-		
-		if(playwright != null)
-			playwright.close();
+
+        if (context.get() != null)
+            context.get().close();
+
+        if (browser.get() != null)
+            browser.get().close();
+
+        if (playwright.get() != null)
+            playwright.get().close();
 	}
 
 }
